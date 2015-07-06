bd7d90b3dcea2117be141af0d3f0f3c8d19b9daa
Patrice Freydiere <frett27@gmail.com>
2015-07-06 21:40:20 +0200

    merge the 0.4-SNAPSHOT to master, add a street definition for extracting all the streets

---
+++ scripts/streets.groovy
@@ -0,0 +1,46 @@
+import com.esri.core.geometry.Geometry
+import com.esri.core.geometry.MultiPath
+import com.esri.core.geometry.Polygon
+import com.poc.osm.model.OSMEntity
+import com.poc.osm.model.OSMEntityGeometry
+
+
+// construction de la chaine
+builder.build(osmstream) {
+
+	// défini un flux de sortie, et description de la structure
+	sortie = gdb(path : var_gdb) {
+		featureclass("streets", ESRI_GEOMETRY_POLYLINE,"WGS84") {
+			_integer('id')
+			_text("type")
+			_text("name")
+		}
+	}
+
+	// a stream
+	b = stream(osmstream, label:"streets") {
+
+		filter {
+			 e -> e instanceof OSMEntity &&
+			   e.geometryType == Geometry.Type.Polyline  && e.getFields() && e.getFields().containsKey("highway")
+		}
+		
+		transform {  e ->
+			String t = e.getFields().get("highway")
+			String name = e.getFields()?.get("name")
+			e.getFields()?.clear()
+			e.setValue("id",e.id)
+			e.setValue("name", name)
+			e.setValue("type", t)
+			return e;
+		}
+
+	}
+
+	
+	// flux de sortie
+	out(streams : [b], gdb : sortie, tablename:"streets")
+
+	
+}
+
