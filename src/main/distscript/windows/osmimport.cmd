3843976d652a1040efe24347fef58879f2a53d3b
Patrice Freydiere <frett27@gmail.com>
2016-03-06 16:24:26 +0100

    fix windows loading

--- src/main/distscript/windows/osmimport.cmd
+++ src/main/distscript/windows/osmimport.cmd
@@ -4,5 +4,5 @@ SET SCRIPT_DIR=%~dp0
 
 PATH %SCRIPT_DIR%/jre/bin;%SCRIPT_DIR%;%PATH%
 
-java -Xmx6g -jar %SCRIPT_DIR%/osmtoolsreader-all-0.5-SNAPSHOT.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Xmx6g -jar %SCRIPT_DIR%/osmtoolsreader-all-0.5-SNAPSHOT.jar %*
 
